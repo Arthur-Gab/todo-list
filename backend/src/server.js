519c9c27401d34345405411828ab686d8d3364cf
Arthur-Gab <arthurgabriel933@gmail.com>
2023-09-04 18:57:16 -0300

    Habilitado o CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 mongoose.connect(
 	'mongodb+srv://arthur:mongoDBAPI@cluster0.zgpreiu.mongodb.net/todoList?retryWrites=true&w=majority'
@@ -8,6 +9,7 @@ mongoose.connect(
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
